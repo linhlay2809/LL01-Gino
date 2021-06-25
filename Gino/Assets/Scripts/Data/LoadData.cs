e14b193469642d50b317d4dee7d7be20584d4918
Đặng Quang Linh <linhlay2809@gmail.com>
2021-06-26 02:25:12 +0700

    Add Pause UI

---
+++ Gino/Assets/Scripts/Data/LoadData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using UnityEngine.SceneManagement;
+
+public class LoadData : MonoBehaviour
+{
+    public string nextScene;
+
+    private string KEY_DATA = "unitdata";
+
+    private void Awake()
+    {
+        LoadUnitData();
+
+        // Chuyển sang Scene khác sau khi load dữ liệu xong
+        SceneManager.LoadScene(nextScene);
+    }
+
+    private void LoadUnitData()
+    {
+        //Lấy dữ liệu dạng string ở PlayerPrefs
+        string s = PlayerPrefs.GetString(KEY_DATA);
+
+        // Nếu chuỗi string null hoặc rỗng thì sẽ tạo một data mới với các giá trị mặc định
+        if (string.IsNullOrEmpty(s))
+        {
+            UnitData.instance = new UnitData();
+            return;
+        }
+
+        // Dùng JsonDotNet convert dữ liệu từ string sang object
+        UnitData.instance = JsonConvert.DeserializeObject<UnitData>(s);
+    }
+}
---
+++ Gino/Assets/Scripts/Data/UnitData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class UnitData
+{
+    public float hp;
+    public int level;
+
+    private string KEY_DATA = "unitdata";
+
+    public static UnitData instance;
+    // Đặt các giá trị mặc định
+    public UnitData()
+    {
+        hp = 100f;
+        level = 1;
+    }
+
+    // Save dữ liệu
+    public void Save(BaseUnit unit)
+    {
+        hp = unit.hp;
+        level = unit.level;
+
+        // Convert dữ liệu sang dạng string
+        string s = JsonConvert.SerializeObject(this);
+
+        // Dùng PlayerPrefs lưu dữ liệu lại
+        PlayerPrefs.SetString(KEY_DATA, s);
+    }
+}
