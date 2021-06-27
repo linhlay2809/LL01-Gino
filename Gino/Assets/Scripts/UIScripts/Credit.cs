7dfbe2186d0f9ce043535b415807c215dd14e0ca
Đặng Quang Linh <linhlay2809@gmail.com>
2021-06-28 03:03:09 +0700

    Add Credit
    Fix resolution

---
+++ Gino/Assets/Scripts/UIScripts/Credit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credit : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TheEnd();
+        
+    }
+    public void TheEnd()
+    {
+        anim.SetBool("End", true);
+        StartCoroutine(DelayEnd());
+    }
+    IEnumerator DelayEnd()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Menu");
+    }
+}
