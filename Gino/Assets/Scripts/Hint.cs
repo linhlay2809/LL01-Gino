a63ecb494b3298f2d68b0c5253d7a015d26e91b5
Đặng Quang Linh <linhlay2809@gmail.com>
2022-05-07 23:10:18 +0700

    Last Update (05/07/2022)

--- Gino/Assets/Scripts/Hint.cs
+++ Gino/Assets/Scripts/Hint.cs
@@ -6,15 +6,35 @@ using UnityEngine.SceneManagement;
 public class Hint : MonoBehaviour
 {
     public Animator anim;
+    public GameObject pressEnter;
+    public bool isSpace = false;
     // Hiện chuỗi ký tự nhập vào lên Text
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.isTrigger == false)
         {
-            Time.timeScale = 0f;
             anim.SetBool("Show", true);
+            StartCoroutine(EnterDelay());
         }
-    }    
+    }
+    protected void Update()
+    {
+        if (isSpace)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                anim.SetBool("Show", false);
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+    IEnumerator EnterDelay()
+    {
+        yield return new WaitForSeconds(3f);
+        pressEnter.SetActive(true);
+        isSpace = true;
+    }
+
     public void CloseHint()
     {
         Time.timeScale = 1f;
